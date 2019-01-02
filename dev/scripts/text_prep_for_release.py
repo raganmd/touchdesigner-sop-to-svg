22a0f09c8688fff5473611575c43088b73300668
Matthew Ragan <matthew.ragan@obscuradigital.com>
2019-01-02 11:33:18 -0800

    Removed template Work on release process Release code clean-up

--- dev/scripts/text_prep_for_release.py
+++ dev/scripts/text_prep_for_release.py
@@ -1,36 +1,34 @@
-base_ext_tox 		= op('base_hueControl')
-version 		    = op('table_version')[0,0].val
-reset_color 	    = (0.545, 0.545, 0.545)
-save_loc 		    = '../release/base_hueControl.tox'
-ext_file 		    = 'hueControlEXT'
-
-ext_ops             = base_ext_tox.findChildren(tags=['EXT', 'MOD'])
-
-destroy_ops         = [ 'svg_icon',
-                      'transform1']
+release_pars_op = op('button_save_for_release')
+base_release_tox = release_pars_op.par.Targetoperator
+version = 'v.{}'.format(release_pars_op.par.Version)
+reset_color = (0.545, 0.545, 0.545)
+save_loc = '{loc}/{name}.tox'.format(loc=release_pars_op.par.Savelocation, name=release_pars_op.par.Toxname)
+destory_tags = release_pars_op.par.Destroytags.val.split(",")
+mods_and_ext_ops = base_release_tox.findChildren(tags=['EXT', 'MOD'])
+destroy_ops = base_release_tox.findChildren(tags=destory_tags)
 
 # save tox
 # set version
-base_ext_tox.par.Version 					= version
+base_release_tox.par.Version = version
 
-for each in ext_ops:
+for each in mods_and_ext_ops:
     # remove path par for ext
-    each.par.file                       = ''
+    each.par.file = ''
     # turn off loading on start
-    each.par.loadonstart                = False
+    each.par.loadonstart = False
 
 # remove path for extenral tox
-base_ext_tox.par.externaltox 				= ''
+base_release_tox.par.externaltox = ''
 
 # set the color to something neutral
-base_ext_tox.color 						= reset_color
+base_release_tox.color = reset_color
 
 # lock the tox for the icon
-base_ext_tox.op('null_icon').lock 			= True
+base_release_tox.op('null_icon').lock = True
 
 # destroy all dev ops
 for each in destroy_ops:
-    base_ext_tox.op(each).destroy()
+    base_release_tox.op(each).destroy()
 
 # save the tox
-base_ext_tox.save(save_loc)
+base_release_tox.save(save_loc)
