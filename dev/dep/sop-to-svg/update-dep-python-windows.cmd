99b6ffa4b250ecbe044477b4228b05c0dc4944ef
Matthew Ragan <matthew.ragan@obscuradigital.com>
2019-01-02 10:13:12 -0800

    removed installation generated files

--- dev/dep/sop-to-svg/update-dep-python-windows.cmd
+++
@@ -1,7 +0,0 @@
-:: Update dependencies
-
-:: make sure pip is up to date
-python -m pip install --upgrade pip
-
-:: install requirements
-pip install -r C:/projects/ragan/touchdesigner/touchdesigner-sop-to-svg/dev/dep/sop-to-svg/requirements.txt --target="C:/projects/ragan/touchdesigner/touchdesigner-sop-to-svg/dev/dep/python"
