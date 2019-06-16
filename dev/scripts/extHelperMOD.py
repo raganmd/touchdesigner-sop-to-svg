99587c080713ccda51cbcf7241310237adb76a6e
Matthew Ragan <raganmd@gmail.com>
2019-06-16 00:24:06 -0700

    Alignment Check
    Standard python installation script #118gr0
    removed extra dir #10b5db
    update release tox #118gr3

--- dev/scripts/extHelperMOD.py
+++ dev/scripts/extHelperMOD.py
@@ -2,11 +2,13 @@ import os
 import subprocess
 import platform
 
-def Check_dep():
+def Check_dep(debug=False):
 	'''
-		This is a sample method.
+		Check for dependencies and project path.
 		
-		This sample method is intended to help illustrate what method docstrings should look like.
+		This method checks to see if the path for the project is included in sys.path.
+		This will ensure that the python modules used in the project will 
+		be respected by the Touch.
 		                
 		Notes
 		---------------
@@ -14,31 +16,46 @@ def Check_dep():
 		
 		Args
 		---------------
-		None
+		debug (bool):
+		> a bool to allow for us to print out the content of sys.path
 								
 		Returns
 		---------------
 		None
 	'''
 
-	Dep_path 		= '{}/dep/python/'.format(project.folder)
+	# our path for all non-standard python modules
+	dep_path 		= '{}/dep/python/'.format(project.folder)
 
-	if Dep_path in sys.path:
+	# if our path is already present we can skip this step
+	if dep_path in sys.path:
 		pass
 
+	# insert the python path into our sys.path
 	else:
-		sys.path.insert(0, Dep_path)
+		sys.path.insert(0, dep_path)
 
-	for each in sys.path:
-		print(each)
+	# print each path in sys.path if debug is true:
+	if debug:
 
-	return
+		for each in sys.path:
+			print(each)
+	else:
+		pass
 
-def Check_dep_path():
+	pass
+
+def Install_python_external():
 	'''
-		This method checks for and creates a dep path.
+		Check and install any external modules.
 		
-		More here shortly.
+		This method will go through all the necessary steps to enesure
+		that our external modules are loaded into our project specific
+		location. This approach assumes that external libraries should be 
+		housed with the project, rather than with the standalone python
+		installation, or with the Touch Installation. This ensrues a more consistent, 
+		reliable, and portable approach when working with non-standard python
+		modules. 
 		                
 		Notes
 		---------------
@@ -58,7 +75,6 @@ def Check_dep_path():
 	scripts_reqs_path 	= '{proj}/dep/{name}'.format(proj=project.folder, name=parent().par.Name)
 	requirements 		= '{}/requirements.txt'.format(scripts_reqs_path)
 	reqs_dat 			= op('reqs')
-	phue_path 			= '{}/dep/python/phue.py'.format(project.folder)
 	win_py_dep 			= '{}/update-dep-python-windows.cmd'.format(scripts_reqs_path)
 	mac_py_dep 			= '{}/update-dep-python-mac.sh'.format(scripts_reqs_path)
 
@@ -113,6 +129,7 @@ def Check_dep_path():
 		# successfully installed our python dependencies
 		if len(os.listdir(python_path)) == 0:
 			subprocess.Popen([win_py_dep])
+
 		else:
 			pass				
 	# on mac
@@ -122,11 +139,18 @@ def Check_dep_path():
 		mac_file.write(mac_py_txt)
 		mac_file.close()
 
+		# change file permissions for the file
+		subprocess.call(['chmod', '755', mac_py_dep])
+
+		# change file to be executable
+		subprocess.call(['chmod', '+x', mac_py_dep])
+
 		# check to see if there is anything in the python dep dir
 		# for now we'll assume that if there are files here we
 		# successfully installed our python dependencies
 		if len(os.listdir(python_path)) == 0:
-			subprocess.Popen([mac_py_dep])
+			print("Running Install Script")
+			subprocess.Popen(["open", "-a", "Terminal.app", mac_py_dep])
 		else:
 			pass
 
@@ -136,9 +160,35 @@ def Check_dep_path():
 	return
 
 def win_dep(requirementsPath, targetPath):
+	'''
+		Format text for command line execution.
+		
+		This method returns a formatted script to be executed by windows to
+		both upgrade pip, and install any modules listed in the requirements
+		DAT.
+		                
+		Notes
+		---------------
+		'self' does not need to be included in the Args section.
+		
+		Args
+		---------------
+		requirementsPath (str):
+		> a string path to the requirements txt doc
+
+		targetPath (str):
+		> a string path to the target installation directory
+
+		Returns
+		---------------
+		formatted_win_txt (str):
+		> the formatted text for a .cmd file for automated installation
+	'''
 	win_txt = ''':: Update dependencies
+
 :: make sure pip is up to date
-python -m pip install --upgrade pip
+python -m pip install --user --upgrade pip
+
 :: install requirements
 pip install -r {reqs}/requirements.txt --target="{target}"'''
 
@@ -146,21 +196,50 @@ pip install -r {reqs}/requirements.txt --target="{target}"'''
 	
 	return formatted_win_txt
 
+
 def mac_dep(requirementsPath, targetPath):
+	'''
+		Format text for command line execution.
+		
+		This method returns a formatted script to be executed by macOS to
+		both upgrade pip, and install any modules listed in the requirements
+		DAT.
+		                
+		Notes
+		---------------
+		'self' does not need to be included in the Args section.
+		
+		Args
+		---------------
+		requirementsPath (str):
+		> a string path to the requirements txt doc
+
+		targetPath (str):
+		> a string path to the target installation directory
+
+		Returns
+		---------------
+		formatted_mac_txt (str):
+		> the formatted text for a .sh file for automated installation
+	'''
 	mac_txt = '''
 #!/bin/bash 
+
 dep=$(dirname "$0")
 pythonDir=/python
+
 # change current direcotry to where the script is run from
 dirname "$(readlink -f "$0")"
-# permission to run the file
-sudo chmod 755 udpate-dep-python-mac.sh
+
 # fix up pip with python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
+
 # Update dependencies
+
 # make sure pip is up to date
-python3 -m pip install --upgrade pip
+python3 -m pip install --user --upgrade pip
+
 # install requirements
 python3 -m pip install -r {reqs}/requirements.txt --target={target}'''
 	formatted_mac_txt = mac_txt.format(reqs=requirementsPath, target=targetPath)
